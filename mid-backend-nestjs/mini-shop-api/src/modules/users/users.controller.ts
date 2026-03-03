feebc429564a472ef574e65e6801deaa07911374
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-03-03 20:30:27 +0600

    Modify

--- mid-backend-nestjs/mini-shop-api/src/modules/users/users.controller.ts
+++ mid-backend-nestjs/mini-shop-api/src/modules/users/users.controller.ts
@@ -6,19 +6,19 @@ import { UserEntity } from './entities/user.entity';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // GET /users - get all users
+  //! GET /users - get all users
   @Get()
   async getAll(): Promise<UserEntity[]> {
     return this.userService.findAll();
   }
 
-  // GET /users/:id - get a user by ID
+  //! GET /users/:id - get a user by ID
   @Get(':id')
   async getOne(@Param('id', ParseIntPipe) id: number): Promise<UserEntity | null> {
     return this.userService.findOne(id);
   }
 
-  // POST /users - create a new user
+  //! POST /users - create a new user
   @Post()
   async create(@Body() userData: Partial<UserEntity>): Promise<UserEntity> {
     return this.userService.create(userData);
